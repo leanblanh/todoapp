0e9270b180a832155750eb91852d9a08135f2d84
Lean Blanh <leanblanh@gmail.com>
2025-06-15 19:21:24 -0300

    feat: first commit

---
+++ components/app/nav-bar.vue
@@ -0,0 +1,17 @@
+<template>
+     <nav class="container">
+            <ul>
+                <li>
+                    <NuxtLink
+                        to="/">
+                        <strong>Todo App</strong>                    
+                    </NuxtLink>
+                </li>
+            </ul>
+            <ul>
+                <li>
+                    <NuxtLink :to="{ name: 'create'}">Create Task</NuxtLink>
+                </li>                
+            </ul>
+    </nav>
+</template>
---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+	modules: ["@nuxt/eslint", "@nuxt/fonts"],
+	devtools: { enabled: true },
+	css: ["@picocss/pico"],
+	compatibilityDate: "2025-05-15",
+	eslint: {
+		config: {
+			stylistic: {
+				semi: true,
+				quotes: 'double',
+				commaDangle: "always-multiline",
+				indent: "tab",
+			},
+		},
+	},
+});
